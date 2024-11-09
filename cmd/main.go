357287b1bc13fec7124bdae0b06be7ec0ec0801d
pchkauu <pchkauu@gmail.com>
2024-11-10 00:11:32 +0300

    [add] Добавлен новый формат логов для локального окружения

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"golang.org/x/exp/slog"
+
 	"os"
 	"url_shortener_svc/internal/config"
-	"url_shortener_svc/internal/lib/logger/sl"
-	"url_shortener_svc/internal/storage/sqlite"
+	"url_shortener_svc/internal/lib/logger/handlers/slogpretty"
 )
 
 const (
@@ -27,19 +29,18 @@ func main() {
 	log.Info("starting url shortener service", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
 
-	storage, err := sqlite.New(cfg.StoragePath)
-	if err != nil {
-		log.Error("failed to init storage", sl.Err(err))
-		os.Exit(1)
-	}
+	//storage, err := sqlite.New(cfg.StoragePath)
+	//if err != nil {
+	//	log.Error("failed to init storage", sl.Err(err))
+	//	os.Exit(1)
+	//}
 
-	err = storage.DeleteURL("google")
-	if err != nil {
-		log.Error("failed to get url", sl.Err(err))
-		os.Exit(1)
-	}
+	router := chi.NewRouter()
 
-	log.Info("delete url successful")
+	router.Use(middleware.RequestID)
+	//router.Use(logger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 }
 
 func setupLogger(env string) (*slog.Logger, error) {
@@ -47,9 +48,7 @@ func setupLogger(env string) (*slog.Logger, error) {
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -63,4 +62,16 @@ func setupLogger(env string) (*slog.Logger, error) {
 	}
 
 	return log, nil
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
