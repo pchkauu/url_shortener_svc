32814c590b3c65f001d262daa60b5e72041ab9d9
@bwg.it576 <bwg.it576@gmail.com>
2024-11-09 17:44:15 +0300

    [add] Инициализация логгера

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"url_shortener_svc/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log, err := setupLogger(cfg.Env)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info("starting url shortener service", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+}
+
+func setupLogger(env string) (*slog.Logger, error) {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		return nil, fmt.Errorf("invalid environment: %s", env)
+	}
+
+	return log, nil
 }
